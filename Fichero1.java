852e3a2a3a74007adb5ca48e9d6c8cd514b931fd
Andony Ng <andony.ng.11@gmail.com>
2022-01-21 16:41:00 +0100

    Se añade el bucle test al programa y se crea el archivo html de prueba

--- Fichero1.java
+++ Fichero1.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Fichero1 {
 	
 	public static void main(String[] Args) {
 		
 		System.out.println("Fichero1");
 		System.out.println("Testing");
+		
+		Scanner read = new Scanner(System.in);
+		int opcion;
+
+		//Bucle de test
+		do {
+
+			System.out.println("Introduzca 0 para terminar el programa, de lo contrario este continuará.");
+			opcion=read.nextInt();
+
+		} while (opcion!=0);
 		
 	}
 	
